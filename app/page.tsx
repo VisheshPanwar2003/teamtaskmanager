4ab019ae0031de89fee07072a21bc4174ad12231
VisheshPanwar2003 <142052984+VisheshPanwar2003@users.noreply.github.com>
2026-05-07 23:46:02 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Home() {
+
+  useEffect(() => {
+
+    window.location.href = "/index.html";
+
+  }, []);
+
+  return null;
+}
